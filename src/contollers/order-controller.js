4fc671fdbf89c20dfa000e435c572675d441e010
Tr-Junior <tarcisio.junior1406@gmail.com>
2024-08-29 22:45:38 -0300

    update order-controller.js

--- src/contollers/order-controller.js
+++ src/contollers/order-controller.js
@@ -33,6 +33,59 @@ exports.getSales = async (req, res, next) => {
     }
 }
 
+// exports.post = async (req, res, next) => {
+//     try {
+//         const token = req.body.token || req.query.token || req.headers['x-access-token'];
+//         const data = await authService.decodeToken(token);
+//         const number = guid.raw().substring(0, 6);
+
+//         // Validação das formas de pagamento
+//         let totalPayments = 0;
+//         req.body.sale.payments.forEach(payment => {
+//             totalPayments += payment.amount;
+//         });
+
+//         if (totalPayments !== req.body.sale.total) {
+//             return res.status(400).send({
+//                 message: 'A soma das formas de pagamento deve ser igual ao total da venda.'
+//             });
+//         }
+
+//         await repository.create({
+//             customer: data._id,
+//             number: number,
+//             client: req.body.client,
+//             sale: {
+//                 items: req.body.sale.items,
+//                 discount: req.body.sale.discount,
+//                 total: req.body.sale.total,
+//                 payments: req.body.sale.payments // Recebe o array de pagamentos
+//             }
+//         });
+
+//         await entrance.create({
+//             numberOfOrder: number,
+//             value: req.body.sale.total
+//         });
+
+//         req.body.sale.items.forEach(async (e) => {
+//             const products = await product.getById(e.product);
+//             product.update(products, {
+//                 quantity: products.quantity - e.quantity,
+//             });
+//         });
+
+//         res.status(201).send({
+//             message: 'Venda efetuada com sucesso'
+//         });
+//     } catch (e) {
+//         res.status(500).send({
+//             message: 'Falha ao processar a requisição'
+//         });
+//     }
+// };
+
+
 exports.post = async (req, res, next) => {
     try {
 
