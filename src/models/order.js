87cc9a57674ec26e35e5529cc97df2d8824de4ef
Trjunior <tarcisio.junior1406@gmail.com>
2024-08-31 12:07:37 -0300

    update order.js

--- src/models/order.js
+++ src/models/order.js
@@ -3,67 +3,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const moment = require('moment-timezone');
 
-// const schema = new Schema({
-//     customer: {
-//         type: mongoose.Schema.Types.ObjectId,
-//         ref: 'Customer'
-//     },
-//     number: {
-//         type: String,
-//         required: true
-//     },
-//     client:{
-//         type: String,
-//         trim: true
-//     },
-//     sale: {
-//         items: [{
-//             product: {
-//                 type: mongoose.Schema.Types.ObjectId,
-//                 ref: 'Product'
-//             },
-//             title: {
-//                 type: String,
-//                 trim: true
-//             },
-//             quantity: {
-//                 type: Number,
-//                 required: true,
-//                 trim: true
-//             },
-//             price: {
-//                 type: Number,
-//                 required: true,
-//                 trim: true
-//             },
-//            purchasePrice: {
-//                 type: Number,
-//                 required: true,
-//                 trim: true
-//             },
-//         }],
-//         discount: {
-//             type: Number,
-//             trim: true
-//         },
-//         total: {
-//             type: Number,
-//             trim: true
-//         },
-//         formPayment: {
-//             type: String,
-//             trim: true
-//         }
-//     },
-//     createDate: {
-//         type: Date,
-//         required: true,
-//         default: Date.now
-//     },
-// });
-
-// module.exports = mongoose.model('Order', schema);
-
 const PaymentSchema = new Schema({
     method: {
         type: String,
